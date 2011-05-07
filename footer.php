14e82bcde1be839d4c2d98cec1824a9b9fad9b2d
vxsx <vxsx@me.com>
2011-05-07 22:38:30 +0400

    added prettyprint for code snippets

--- footer.php
+++ footer.php
@@ -5,6 +5,9 @@
 	<footer class="blog-footer">
 		<p>&copy; 2010 &mdash; 2011, Вадим Сикора </p>
 	</footer>
+<script type='text/javascript'>
+    prettyPrint();
+</script>
 <script>var _gaq=[['_setAccount','UA-15927085-2'],['_trackPageview']];(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;g.src='//www.google-analytics.com/ga.js';s.parentNode.insertBefore(g,s)}(document,'script'))</script>
 <!--[if lt IE 7 ]>
 <script src="/wp-content/themes/vxsx/js/dd_belatedpng.js"></script>
