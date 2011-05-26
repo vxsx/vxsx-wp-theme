ceb38d61db2ccf312545a144045319882f0add8b
vxsx <vxsx@me.com>
2011-05-26 21:41:56 +0400

    new counter code + centerred layout

--- footer.php
+++ footer.php
@@ -15,13 +15,9 @@
 <script src="/wp-content/themes/vxsx/js/dd_belatedpng.js"></script>
 <script type="text/javascript">DD_belatedPNG.fix('.png');</script>
 <![endif]-->
-<!-- Yandex.Metrika -->
-<script src="//mc.yandex.ru/metrika/watch.js" type="text/javascript"></script>
-<div style="display:none;"><script type="text/javascript">
-try { var yaCounter1578791 = new Ya.Metrika(1578791);yaCounter1578791.clickmap();} catch(e){}
-</script></div>
-<noscript><div style="position:absolute"><img src="//mc.yandex.ru/watch/1578791" alt="" /></div></noscript>
-<!-- /Yandex.Metrika -->
+
+<!-- Yandex.Metrika counter --><div style="display:none;"><script type="text/javascript">(function(w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter1578791 = new Ya.Metrika({id:1578791, clickmap:true}); } catch(e) { } }); })(window, 'yandex_metrika_callbacks');</script></div><script src="//mc.yandex.ru/metrika/watch.js" type="text/javascript" defer="defer"></script><noscript><div><img src="//mc.yandex.ru/watch/1578791" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
+
 <?php wp_footer(); ?>
 </body>
 </html>
