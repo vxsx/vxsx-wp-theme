72d754137e614219b4b3d136deee47b04d163c76
vxsx <vxsx@me.com>
2011-06-13 19:13:12 +0400

    some microdata

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,7 @@
 	<img src="i/vxsx.jpg" alt="Вадим Сикора">
 	<p class="copy"><a href="http://dashamayz.ru" target="_blank">&copy; Dasha Mayz</a></p>
 	-->
-	<p>Привет, меня зовут Вадим Сикора, я веб-технолог, живу в Москве и иногда пишу в этот блог, иногда что-то делаю на <a href="http://github.com/vxsx">гитхабе</a>. Подписывайтесь на <a href="/feed/">RSS</a></p>			
+	<p itemscope itemtype="http://schema.org/Person">Привет, меня зовут <span itemprop="name">Вадим Сикора</a>, я <span itemprop="title">веб-технолог</span>, живу в Москве и иногда пишу в этот блог, иногда что-то делаю на <a href="http://github.com/vxsx">гитхабе</a>. Подписывайтесь на <a href="/feed/">RSS</a></p>			
 	<div id="last-tweet">
 		<div class="arrow"></div>
 		<div class="username"><a href="http://twitter.com/vxsx">vxsx</a></div>
